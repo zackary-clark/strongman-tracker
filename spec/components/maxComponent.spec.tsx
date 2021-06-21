a99810091926278f3e56e3c517f5056bdbbf7b2e
zackary-clark <zackary.a.clark@gmail.com>
2021-06-21 19:57:02 +0000

    Makes GetMaxes call on render, and removes button

--- spec/components/maxComponent.spec.tsx
+++ spec/components/maxComponent.spec.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { render } from "@testing-library/react";
-import { waitFor, fireEvent } from "@testing-library/dom";
+import { waitFor } from "@testing-library/dom";
 import * as WebClient from "../../src/webClient";
 import { MaxComponent } from "../../src/components";
-import { sampleMaxesArray, defaultAxiosResponse, sampleMax } from "../test-helpers/data";
+import { defaultAxiosResponse, sampleMax, sampleMaxesArray } from "../test-helpers/data";
 import { renderWithSnackbar } from "../test-helpers/testUtils";
 
 describe("maxComponent", () => {
     let getMaxesSpy: jest.SpyInstance;
     let postMaxSpy: jest.SpyInstance;
 
-    beforeAll(() => {
+    beforeEach(() => {
         getMaxesSpy = jest.spyOn(WebClient, "getMaxes").mockResolvedValue({
             ...defaultAxiosResponse,
             data: sampleMaxesArray
@@ -25,29 +25,20 @@ describe("maxComponent", () => {
         jest.clearAllMocks();
     });
     
-    describe("GetMaxes button", () => {
-        it("should render GetMaxes button", () => {
-            const { getByTitle } = render(<MaxComponent />);
-            expect(getByTitle("Get Maxes")).toHaveTextContent("Get Maxes");
-        });
-
-        it("should call webclient.getMaxes on 'GetMaxes' click", () => {
-            const { getByTitle } = render(<MaxComponent />);
-            getByTitle("Get Maxes").click();
+    describe("GetMaxes", () => {
+        it("should call webclient.getMaxes on render", () => {
+            render(<MaxComponent />);
             expect(getMaxesSpy).toBeCalled();
         });
 
-        it("should add data to table on 'GetMaxes' click", async () => {
-            const { getByTitle, getByText } = render(<MaxComponent />);
-            getByTitle("Get Maxes").click();
+        it("should add data to table", async () => {
+            const { getByText } = render(<MaxComponent />);
             await waitFor(() => expect(getByText("123456")));
         });
 
         it("should show snackbar when getMaxes fails", async () => {
             getMaxesSpy = jest.spyOn(WebClient, "getMaxes").mockRejectedValue("error");
-            const { getByTitle, getByText } = renderWithSnackbar(<MaxComponent />);
-
-            getByTitle("Get Maxes").click();
+            const { getByText } = renderWithSnackbar(<MaxComponent />);
 
             await waitFor(() => expect(getByText("Network Error!")));
         });
@@ -66,16 +57,6 @@ describe("maxComponent", () => {
             getByTitle("Save").click();
 
             expect(postMaxSpy).toHaveBeenCalled();
-        });
-
-        it("should place new entry in table on 'Add'", async () => {
-            const { getByTitle, getByPlaceholderText, getByText } = render(<MaxComponent />);
-
-            getByTitle("Add").click();
-            fireEvent.change(getByPlaceholderText("Squat"), { target: { value: "225" }});
-            getByTitle("Save").click();
-
-            await waitFor(() => expect(getByText("225")));
         });
 
         it("should show snackbar when save fails", async () => {
--- src/components/maxes/maxComponent.tsx
+++ src/components/maxes/maxComponent.tsx
@@ -1,57 +1,26 @@
 import * as React from "react";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import MaterialTable from "material-table";
-import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
 import { IMax } from "../../data/max";
 import { getMaxes, postMax } from "../../webClient";
 import { SnackbarContext } from "../../context";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-    button: {
-        margin: theme.spacing(1),
-    },
-}));
-
 export function MaxComponent(): React.ReactElement {
-    const classes = useStyles();
     const { onOpenSnackbar } = useContext(SnackbarContext);
     const [ maxes, setMaxes ] = useState<IMax[]>([]);
 
-    const generateTable = (): React.ReactElement => (
-        // TODO: This table looks bad on mobile
-        <MaterialTable
-            title={"One Rep Max Tracker"}
-            columns={[
-                {
-                    title: "Date",
-                    field: "date",
-                    type: "date",
-                },
-                {title: "Squat", field: "squat1RM", type: "numeric"},
-                {title: "Bench", field: "bench1RM", type: "numeric"},
-                {title: "Deadlift", field: "deadlift1RM", type: "numeric"},
-                {title: "OHP", field: "press1RM", type: "numeric"},
-            ]}
-            data={maxes}
-            editable={{
-                onRowAdd: addEntry,
-                onRowDelete: () => {
-                    return new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            resolve();
-                        }, 10);
-                    });
-                },
-                onRowUpdate: () => {
-                    return new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            resolve();
-                        }, 10);
-                    });
-                },
-            }}
-        />
-    );
+    useEffect(() => {
+        getMaxes()
+            .then((res) => res.data)
+            .then((resData) => {
+                console.log(resData);
+                setMaxes(resData);
+            })
+            .catch(e => {
+                console.error("Get Failed");
+                onOpenSnackbar("Network Error!");
+            });
+    }, []);
 
     const addEntry = (newData: IMax): Promise<void | IMax> => {
         return postMax(newData)
@@ -66,27 +35,36 @@ export function MaxComponent(): React.ReactElement {
             });
     };
 
-    const getMaxesOnClick = (): void => {
-        getMaxes()
-            .then((res) => res.data)
-            .then((resData) => setMaxes(resData))
-            .catch(e => {
-                console.error("Get Failed");
-                onOpenSnackbar("Network Error!");
-            });
-    };
-    
     return (
         <div className={"max-container"}>
-            <Button
-                className={`get-maxes ${classes.button}`}
-                onClick={getMaxesOnClick}
-                variant={"contained"}
-                title={"Get Maxes"}
-            >
-                Get Maxes
-            </Button>
-            {generateTable()}
+            <MaterialTable
+                title={"One Rep Max Tracker"}
+                columns={[
+                    {title: "Date", field: "date", type: "date", initialEditValue: new Date()},
+                    {title: "Squat", field: "squat1RM", type: "numeric"},
+                    {title: "Bench", field: "bench1RM", type: "numeric"},
+                    {title: "Deadlift", field: "deadlift1RM", type: "numeric"},
+                    {title: "OHP", field: "press1RM", type: "numeric"},
+                ]}
+                data={maxes}
+                editable={{
+                    onRowAdd: addEntry,
+                    onRowDelete: () => {
+                        return new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                resolve();
+                            }, 10);
+                        });
+                    },
+                    onRowUpdate: () => {
+                        return new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                resolve();
+                            }, 10);
+                        });
+                    },
+                }}
+            />
         </div>
     );
 }
